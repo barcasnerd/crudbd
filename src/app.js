e9146a9cc87019b3fd6b3ec547074a4935ac06dd
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-04-08 01:15:20 -0500

    Conección Node.js y mariadb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express'),
+/*const express = require('express'),
       path = require('path'),
       morgan = require('morgan'),
       mariadb = require('mariadb'),
@@ -34,4 +34,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // starting the server
 app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`);
+});*/
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bienestar',
+    connectionLimit: 5
 });
+pool.getConnection()
+    .then(conn => {
+    
+      conn.query("SELECT 1 as val")
+        .then((rows) => {
+          console.log(rows); //[ {val: 1}, meta: ... ]
+          //Table must have been created before 
+          conn.query("CREATE TABLE myTable (id int, val varchar(255))")
+          return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        })
+        .then((res) => {
+          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err); 
+          conn.end();
+        })
+        
+    }).catch(err => {
+      //not connecteds
+    });
